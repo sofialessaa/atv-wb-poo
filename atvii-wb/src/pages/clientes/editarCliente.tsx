2b5c1ab16f5f9755f537288f24d642cbf8681e4e
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-05-28 18:27:12 -0300

    Update editarCliente.tsx

--- atvii-wb/src/pages/clientes/editarCliente.tsx
+++ atvii-wb/src/pages/clientes/editarCliente.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import '../styles.scss'
 import BarraNavegacao from '../../componentes/barraNavegacao';
 
 export class EditarCliente extends Component {
@@ -15,20 +16,20 @@ export class EditarCliente extends Component {
                         <form>
                             <div className="field">
                                 <label htmlFor="Nome" >Nome:</label>
-                                <input type="text" defaultValue="Taylor"/>
+                                <input type="text" />
                             </div>
                             <div className="field">
                                 <label htmlFor="Sobrenome">Sobrenome:</label>
-                                <input type="text" defaultValue="Swift"/>
+                                <input type="text" />
                             </div>
                             <div className="field">
                                 <label htmlFor="Social">Nome social:</label>
-                                <input type="text" defaultValue="Swift"/>
+                                <input type="text" />
                             </div>
                             <div className="field">
                                 <label htmlFor="Genero">Gênero:</label>
                                 <select name="genero" id="genero">
-                                    <option>Selecione seu gênero</option>
+                                    <option>- Selecione seu gênero -</option>
                                     <option>Feminino</option>
                                     <option>Masculino</option>
                                     <option>Outro</option>
@@ -36,19 +37,19 @@ export class EditarCliente extends Component {
                             </div>
                             <div className="field">
                                 <label htmlFor="cpf">CPF:</label>
-                                <input type="text" defaultValue="131.313.131-31"/>
+                                <input type="text" />
                             </div>
                             <div className="field">
                                 <label htmlFor="rg">RG:</label>
-                                <input type="text" defaultValue="13.131.313-1"/>
+                                <input type="text" />
                             </div>
                             <div className="field">
                                 <label htmlFor="Telefone">Telefone:</label>
-                                <input type="text" defaultValue="(12) 99812-3767"/>
+                                <input type="text" />
                             </div>
                             <div className='button-editar'>
-                                <Button className="submit-editar" variant="outline-dark" type='submit'>Editar</Button>{' '}
-                                <Button className="submit-editar" variant="outline-dark" href='/clientes'>Voltar</Button>
+                                <Button className="submit-editar" type='submit'>Editar</Button>{' '}
+                                <Button className="submit-editar" href='/'>Voltar</Button>
                             </div>
                         </form>
                     </div>
