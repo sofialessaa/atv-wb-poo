ebe87f30691ddfc74d030e696ac4b5bf0af7c354
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-05-28 18:40:13 -0300

    Update comprarServico.tsx

--- atvii-wb/src/pages/servicos/comprarServico.tsx
+++ atvii-wb/src/pages/servicos/comprarServico.tsx
@@ -18,7 +18,7 @@ export class ComprarServicos extends Component {
                                 <input type="text" />
                             </div>
                             <div className="field">
-                                <label htmlFor="Sobrenome">Número do serviço desejado:</label>
+                                <label htmlFor="Sobrenome">ID ou nome do serviço:</label>
                                 <input type="text" />
                             </div>
                             <div className="field">
@@ -31,11 +31,13 @@ export class ComprarServicos extends Component {
                                     <option>4</option>
                                     <option>5</option>
                                     <option>6</option>
+                                    <option>7</option>
+                                    <option>8</option>
+                                    <option>9</option>
+                                    <option>10</option>
                                 </select>
                             </div>
-                            <Button className="submit" variant="outline-dark" type="submit">
-                                Comprar
-                            </Button>{" "}
+                            <Button className="submit" type="submit">Comprar</Button>{" "}
                         </form>
                     </div>
                 </main>
