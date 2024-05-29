2a81e3cd1bd028be0d608db59137bb62311e14c5
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-05-28 23:56:17 -0300

    Update editarProduto.tsx

--- atvii-wb/src/pages/produtos/editarProduto.tsx
+++ atvii-wb/src/pages/produtos/editarProduto.tsx
@@ -13,21 +13,6 @@ export class EditarProduto extends Component {
                 <main>
                     <h1>Editar Produto</h1>
                     <div className="forms">
-                        <form>import React, { Component } from 'react';
-import CurrencyInput from 'react-currency-input-field';
-import { Button } from 'react-bootstrap';
-import BarraNavegacao from '../../componentes/barraNavegacao';
-
-export class EditarProduto extends Component {
-    render() {
-        return (
-            <section>
-                <header>
-                    <BarraNavegacao/>
-                </header>
-                <main>
-                    <h1>Editar Produto</h1>
-                    <div className="forms">
                         <form>
                             <div className="field">
                                 <label htmlFor="Produto">Produto:</label>
