f7ee3f469d43cf78c180ba25d52f1dfaf2a2b0ab
sofialessaa <sofia.lessa@fatec.sp.gov.br>
2024-06-16 22:56:06 -0300

    Update atvii-wb

--- atvii-wb/src/pages/servicos/comprarServico.tsx
+++ atvii-wb/src/pages/servicos/comprarServico.tsx
@@ -21,22 +21,6 @@ export class ComprarServicos extends Component {
                                 <label htmlFor="Sobrenome">ID ou nome do serviço:</label>
                                 <input type="text" />
                             </div>
-                            <div className="field">
-                                <label htmlFor="Genero">Quantidade:</label>
-                                <select name="genero" id="genero">
-                                    <option>- Selecione a quantidade -</option>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                    <option>6</option>
-                                    <option>7</option>
-                                    <option>8</option>
-                                    <option>9</option>
-                                    <option>10</option>
-                                </select>
-                            </div>
                             <Button className="submit" type="submit">Comprar</Button>{" "}
                         </form>
                     </div>
